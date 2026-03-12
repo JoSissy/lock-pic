a90c1341341acee847d4ba1db95e3b9588e9a432
JoSissy <joliesissy2000@gmail.com>
2026-03-12 16:51:28 -0500

    gitignore

--- main.go
+++ main.go
@@ -1,23 +1,13 @@
 package main
 
 import (
-	"context"
-
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-	ctx := context.Background()
-
 	// Create a new Echo instance
 	e := echo.New()
-
-	// Initialize Google Drive service
-	err := initGoogleDrive(ctx)
-	if err != nil {
-		panic(err)
-	}
 
 	// Middleware
 	e.Use(middleware.Logger())
